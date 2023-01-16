771b2f53a15e0d67505657f8560e630b298858c2
heather kruszewski <heather.k.tech@gmail.com>
2023-01-16 12:51:09 -0500

    commit tree cc6d49c93e2f105f3f8af012b02f6a0f67746556

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, list) {
+    return drivers.filter(
+        (driver) => driver.toLowerCase() === list.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, list) {
+    return drivers.filter(
+        (driver) => driver.toLowerCase().indexOf(list.toLowerCase()) === 0
+    )
+}
+  
+function matchName(drivers, list) {
+    return drivers.filter((driver) => driver.name === list)
+}
+
